ff2580a4575d244ad841fa7358b0d38e75e49c84
dante <lkstudio.dante@gmail.com>
2022-12-28 10:35:22 +0900

    no message

--- Framework/Structure/Example/Practice/Classes/Practice_01/Practice_01.cpp
+++ Framework/Structure/Example/Practice/Classes/Practice_01/Practice_01.cpp
@@ -1,7 +1,22 @@
 #include "Practice_01.h"
 
+#define P01_01
+#define P01_02
+
 namespace P01 {
+#if defined P01_01
+
+#elif defined P01_02
+
+#endif // #if defined P01_01
+
 	int Practice_01(int argc, char* args[]) {
+#if defined P01_01
+
+#elif defined P01_02
+
+#endif // #if defined P01_01
+
 		return 0;
 	}
 }

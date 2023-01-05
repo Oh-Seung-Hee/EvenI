58c516f150de49a301db82455c963973db9f1bd6
dante <lkstudio.dante@gmail.com>
2023-01-06 08:01:54 +0900

    no message

--- Framework/Structure/Example/Practice/Classes/Practice.cpp
+++ Framework/Structure/Example/Practice/Classes/Practice.cpp
@@ -1,6 +1,8 @@
 #include "Practice_01/Practice_01.h"
+#include "Practice_02/Practice_02.h"
 
 /** 메인 함수 */
 int main(int argc, char* args[]) {
-	return P01::Practice_01(argc, args);
+	//return P01::Practice_01(argc, args);
+	return P02::Practice_02(argc, args);
 }
---
+++ Framework/Structure/Example/Practice/Classes/Practice_02/Practice_02.cpp
@@ -0,0 +1,22 @@
+#define P02_01
+#define P02_02
+
+#include "Practice_02.h"
+
+namespace P02 {
+#if defined P02_01
+
+#elif defined P02_02
+
+#endif // #if defined P02_01
+
+	int Practice_02(int argc, char* args[]) {
+#if defined P02_01
+
+#elif defined P02_02
+
+#endif // #if defined P02_01
+
+		return 0;
+	}
+}
